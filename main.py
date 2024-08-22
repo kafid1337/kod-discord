94d993cfd33bfb22e5ed240954c636ea16552631
kafid1337 <ahmetovt380@gmail.com>
2024-08-22 12:11:47 +0300

    bot hello

---
+++ main.py
@@ -0,0 +1,41 @@
+import discord
+from bot_logic import *
+
+
+
+
+
+
+
+# Переменная intents - хранит привилегии бота
+intents = discord.Intents.default()
+# Включаем привелегию на чтение сообщений
+intents.message_content = True
+# Создаем бота в переменной client и передаем все привелегии
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hi!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\\U0001f642")
+    if message.content.startswith('how are you'):
+        await message.channel.send("good")
+    elif message.content.startswith('%password'):
+        await message.channel.send("your password" + gen_pass(10))
+    else:
+        await message.channel.send(message.content) 
+
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+    
+
+client.run("your token")
